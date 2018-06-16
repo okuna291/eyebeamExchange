efa5abed2b259cd818b8f720c6f10516eddd1378
Ayodamola Okunseinde <aokunseinde@gmail.com>
2018-06-16 02:14:46 -0400

    --

--- test.py
+++
@@ -1,9 +0,0 @@
-import smtplib
- 
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.starttls()
-server.login("raspberrypicallhome@gmail.com'", "picallhome")
- 
-msg = "YOUR MESSAGE!"
-server.sendmail("raspberrypicallhome@gmail.com", "raspberrypicallhome@gmail.com", msg)
-server.quit()
